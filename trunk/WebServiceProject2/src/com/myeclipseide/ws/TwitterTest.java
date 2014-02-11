c238a4fdce78b751e1de77a555c3bae8ac45a4af
marcela.domingues@gmail.com <marcela.domingues@gmail.com@86c7e77b-fbba-4aad-4f5b-972f0705c337>
2014-02-11 20:43:46 +0000

    Funções getNegativeMentions e getPositiveMentions.

--- trunk/WebServiceProject2/src/com/myeclipseide/ws/TwitterTest.java
+++ trunk/WebServiceProject2/src/com/myeclipseide/ws/TwitterTest.java
@@ -1,7 +1,7 @@
 package com.myeclipseide.ws;
 
+import java.util.ArrayList;
 import java.util.List;
-
 
 import twitter4j.Query;
 import twitter4j.QueryResult;
@@ -12,34 +12,20 @@ import twitter4j.TwitterFactory;
 import twitter4j.auth.AccessToken;
 
 
-
-
 public class TwitterTest  {
-	public static void main(String[] args) {
+	
+	public static void main(String[] args) throws TwitterException {
+		//System.out.println("==========TIMELINE==========");
 		//printTimeline(getTwitter());
-		getTweets(getTwitter());
+		//System.out.println("==========BUSCA=============");
+		//getTweets(getTwitter(),"@vivo");
+		//System.out.println("==========BUSCA NEGATIVA=============");
+		//getNegativeMentions(getTwitter(),"@vivo");
+		System.out.println("==========BUSCA POSITIVA=============");
+		getPositiveMentions(getTwitter(),"@vivo");
 	}
- 
-	/**
-	 * Get Twitter object.
-	 * 
-	 * @return Twitter object.
-	 */
-	public static Twitter getTwitter() {
-		Twitter twitter = new TwitterFactory().getInstance();
- 
-		// Set access
-		twitter.setOAuthConsumer("t0i7oSm1ldndO2jqDSVA", "qOtvPyqXH82iltSeVOzxKQGFMK3SNPA17DfWbvkU");
-		twitter.setOAuthAccessToken(new AccessToken("125470540-DEzN20RQe7RZd4kYe3Niq2KkVdZyc4wSGU80Z9gB", "YfJjkyTrgvIsSfHg5YRcY68xt3UyY9jgmk2npVKtumkBL"));
- 
-		return twitter;
-	}
- 
-	/**
-	 * Print out timeline.
-	 * 
-	 * @param twitter Twitter object
-	 */
+	
+	
 	public static void printTimeline(Twitter twitter) {
 	    List<Status> statuses;
 		try {
@@ -54,18 +40,84 @@ public class TwitterTest  {
 		}		
 	}
 	
-	public static void getTweets(Twitter twitter){
+
+	public static Twitter getTwitter() {
+		Twitter twitter = new TwitterFactory().getInstance();
+ 
+		// Set access
+		twitter.setOAuthConsumer("t0i7oSm1ldndO2jqDSVA", "qOtvPyqXH82iltSeVOzxKQGFMK3SNPA17DfWbvkU");
+		twitter.setOAuthAccessToken(new AccessToken("125470540-DEzN20RQe7RZd4kYe3Niq2KkVdZyc4wSGU80Z9gB", "YfJjkyTrgvIsSfHg5YRcY68xt3UyY9jgmk2npVKtumkBL"));
+ 
+		return twitter;
+	}
+ 
+	public static ArrayList<String> getTweets(Twitter twitter, String stringBusca){
+		
+		ArrayList<String> tweets = new ArrayList<String>();
 		
 		try{
-			Query query = new Query("@saraiva");
+			Query query = new Query(stringBusca);
 		    QueryResult result = twitter.search(query);
 		    for (Status status : result.getTweets()) {
-		        System.out.println("@" + status.getUser().getScreenName() + ":" + status.getText());
+		        //System.out.println("@" + status.getUser().getScreenName() + ":" + status.getText());
+		        tweets.add("@" + status.getUser().getScreenName() + ":" + status.getText());
 		    }
+		        
 		}catch (TwitterException e) {
 			e.printStackTrace();
 		}	
 		
+		return tweets;
+	}	
+
+
+	public static void getNegativeMentions(Twitter twitter, String stringBusca){
+		//aqui seriam as palavras de busca negativa
+		String[] palavrasNegativas = {"ruim","péssimo","horrível","não consigo","mal","não","odeio"};
+		
+		ArrayList<String> listaTweetsNegativos = new ArrayList<String>();
+		
+		ArrayList<String> listaTweets = getTweets( getTwitter(),"@vivo");
+		
+		for (String texto : listaTweets){
+			for (String palavraNeg : palavrasNegativas){
+				if (texto.contains(palavraNeg)){
+					if (!listaTweetsNegativos.contains(texto)){
+						listaTweetsNegativos.add(texto);
+					 }
+					
+				}
+			}
+		}
+		System.out.println(listaTweetsNegativos);
+		System.out.println("Menções Negativas:" + listaTweetsNegativos.size());
+		
+		
 	}
+	
+	public static void getPositiveMentions(Twitter twitter, String stringBusca){
+		//aqui seriam as palavras de busca negativa
+		String[] palavrasPositivas = {"bom","maravilha","ótimo","adorei","gostei","adoro","amo","boa"};
+		
+		ArrayList<String> listaTweetsPositivos = new ArrayList<String>();
+		
+		ArrayList<String> listaTweets = getTweets( getTwitter(),"@saraiva");
+		
+		for (String texto : listaTweets){
+			for (String palavraNeg : palavrasPositivas){
+				if (texto.contains(palavraNeg)){
+					if (!listaTweetsPositivos.contains(texto)){
+						listaTweetsPositivos.add(texto);
+					 }
+					
+				}
+			}
+		}
+		System.out.println(listaTweetsPositivos);
+		System.out.println("Menções Positivas:" + listaTweetsPositivos.size());
+		
+		
+	}
+	
 	
 }
